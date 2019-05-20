f609fbb3c20a37c6d24224eb7b3082e4d7daf721
Eremin Ivan <coding.ebola@gmail.com>
2019-05-20 15:19:26 +0300

    test fix

--- anvil_benchmark_test.go
+++ anvil_benchmark_test.go
@@ -26,7 +26,7 @@ func BenchmarkNotation_WithNoSkip(b *testing.B) {
 func BenchmarkNotation_WithForceSkip(b *testing.B) {
 	var r interface{}
 	v := MyType{}
-	a := &Anvil{Mode: NoSkipEmpty, Glue: "."}
+	a := &Anvil{Mode: SkipEmpty, Glue: "."}
 	a.Modifier(v, modifier.Time)
 
 	for n := 0; n < b.N; n++ {
