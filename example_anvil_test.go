11db8c16b26819d54849d11e053bdfcdc52dd4b4
Eremin Ivan <coding.ebola@gmail.com>
2019-08-07 16:05:40 +0300

    update licence

--- example_anvil_test.go
+++ example_anvil_test.go
@@ -1,4 +1,4 @@
-// Copyright 2013 The Go Authors. All rights reserved.
+// Copyright (c) 2019, Ivan Eremin. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
